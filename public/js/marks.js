3b701de2ab233a28f7dae79efcb03aa79eaee251
Data James <61133735+KEVIN1996420@users.noreply.github.com>
2021-01-07 17:47:15 +0700

    fix issue with each students subject dropdown

--- public/js/marks.js
+++ public/js/marks.js
@@ -1,4 +1,4 @@
-
+// Top Buttons
 document.querySelector("#hideData").addEventListener("click", function myFunction() {
     var x = document.querySelector("#studentData") ;
     if (x.style.display === "none") {
@@ -7,7 +7,6 @@ document.querySelector("#hideData").addEventListener("click", function myFunctio
       x.style.display = "none";
     }
   })
-
   document.querySelector("#hideMarks").addEventListener("click", function myFunction() {
     var x = document.querySelector("#marksContainer") ;
     if (x.style.display === "none") {
@@ -16,7 +15,6 @@ document.querySelector("#hideData").addEventListener("click", function myFunctio
       x.style.display = "none";
     }
   })
-
   document.querySelector("#hideEvents").addEventListener("click", function myFunction() {
     var x = document.querySelector("#eventsContainer") ;
     if (x.style.display === "none") {
@@ -25,7 +23,6 @@ document.querySelector("#hideData").addEventListener("click", function myFunctio
       x.style.display = "none";
     }
   })
-
   document.querySelector("#hideRoster").addEventListener("click", function myFunction() {
     var x = document.querySelector("#rosterContainer") ;
     if (x.style.display === "none") {
@@ -34,4 +31,112 @@ document.querySelector("#hideData").addEventListener("click", function myFunctio
       x.style.display = "none";
     }
   }) 
-  examsContainer
+
+
+// Grade buttons to display data
+document.querySelector("#g1").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr1") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  })
+  document.querySelector("#g2").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr2") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+  document.querySelector("#g3").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr3") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+  document.querySelector("#g4").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr4") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+  document.querySelector("#g5").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr5") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+  document.querySelector("#g6").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr6") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+  document.querySelector("#g7").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr7") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+  document.querySelector("#g8").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr8") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+  document.querySelector("#g9").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr9") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+  document.querySelector("#g10").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr10") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+  document.querySelector("#g11").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr11") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+  document.querySelector("#g12").addEventListener("click", function myFunction() {
+    var x = document.querySelector("#gr12") ;
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }) 
+
+  // // this is to hide and show the subjects on the marks dropdown
+  // document.querySelector(".engBtn").addEventListener("click", function myFunction() {
+  //   var x = document.querySelector(".english") ;
+  //   if (x.style.display === "none") {
+  //     x.style.display = "block";
+  //   } else {
+  //     x.style.display = "none";
+  //   }
+  // })  
