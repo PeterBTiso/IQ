fd82d862d3bbbd0f0e99f5d1f9de74e68508586b
Data James <61133735+KEVIN1996420@users.noreply.github.com>
2021-01-09 11:03:26 +0700

    added some style changes to all the view pages

---
+++ public/js/office.js
@@ -0,0 +1,49 @@
+// hide and display side navbar
+
+// function openNav(){
+//     document.getElementById("mySidenav").style.width = "200px";
+// }
+// function closeNav(){
+//     document.getElementById("mySidenav").style.width = "0";
+// }
+// // Hide and display categories
+// function displayOffice(){
+//     var x = document.querySelector(".office");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+// function displayReception(){
+//     var x = document.querySelector(".reception");
+//   if (x.style.display === "none"){
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+// function displayTeacher(){
+//     var x = document.querySelector(".teacher");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+// function displayAssistant(){
+//     var x = document.querySelector(".assistant");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+// function displayStudent(){
+//     var x = document.querySelector(".students");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
