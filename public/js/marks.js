fdc5336e86306099cca2cc0361d7ca689cb263d5
Peter Brania <peterb@chasesoftware.co.za>
2021-02-19 15:44:00 +0200

    Button logic

--- public/js/marks.js
+++ public/js/marks.js
@@ -1,252 +1,67 @@
+//Safer to only run js code once the page is fully loaded
+document.addEventListener("DOMContentLoaded", function(event) {
+  var handlersAdded = []; //for keeping track of which buttons have had handlers added
+  var sections = ['Students', 'Marks', 'Assesments', 'Events', 'Roster'] //store the different page sections NOTE: Make sure naming is consistent: buttons -> hide{section}, containers => {section}Container
 
-document.querySelector("#hideData").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#studentData") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  })
+  //Hide each page section based on the section list above
+  var hideSections = function(){
+    sections.forEach(element => {
+      document.querySelector(`#${element}Container`).classList.add('invisible');
+    });
+  };
+  //call it straight away to start off with a clean/blank page
+  hideSections();
 
-  document.querySelector("#hideMarks").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#marksContainer") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  })
-  document.querySelector("#hideAssesments").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#assesmentsContainer") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  })
-  document.querySelector("#hideEvents").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#eventsContainer") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  })
-  document.querySelector("#hideRoster").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#rosterContainer") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
+  //add the click listener to each of the section buttons
+  document.querySelectorAll(".topToggleBtns button").forEach(btn =>{
+    btn.addEventListener('click', thisbtn =>{
+      hideSections();
+      removeInvisible(`#${btn.id.replace('hide', '')}Container`);
+    })
+  });
 
+  //Hides all tables
+  var hideAllTables = function(){
+    var sectionTypes = ['Avg', 'Marks'];
 
-// Grade buttons to display data
-document.querySelector("#g1").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr1") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  })
-  document.querySelector("#g2").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr2") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#g3").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr3") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#g4").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr4") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#g5").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr5") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#g6").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr6") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#g7").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr7") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#g8").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr8") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#g9").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr9") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#g10").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr10") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#g11").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr11") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#g12").addEventListener("click", function myFunction() {
-    var x = document.querySelector("#gr12") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
+    sectionTypes.forEach(type => {
+      document.querySelectorAll(`.grade${type}`).forEach(div => {
+        //Adds the 'invisible' class to each div in order to hide it
+        div.classList.add('invisible');
+      })
+    })
+  };
 
-  // this is to hide and show the students marks 
-  document.querySelector("#gr1-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr1Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr2-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr2Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr3-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr3Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr4-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr4Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr5-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr5Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr6-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr6Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr7-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr7Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr8-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr8Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr9-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr9Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr10-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr10Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr11-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr11Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
-  document.querySelector("#gr12-marksB").addEventListener("click", function myFunction() {
-    var x = document.querySelector(".gr12Marks") ;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }) 
+  //call it straight away to hide all tables
+  hideAllTables();
 
-  var dt = new Date();
-  document.getElementById("datetime").innerHTML = dt.toLocaleString();
+  //Removes the 'invisible' class for the element sent through in the selector parameter
+  var removeInvisible = function(selector){
+    document.querySelector(selector).classList.remove('invisible');
+  }
 
-  // this is to hide and show the subjects on the marks dropdown
-// let studentsSubjects = document.querySelectorAll("div");
-// // console.log(studentsSubjects)
-// for(let i = 0 ; i <= studentsSubjects.length() ; i++ ){
-//   if(studentsSubjects.classList.contains(`n${i}`))
-//   document.querySelector(`n${i}`).toggle().display();
-//   console.log(i)
-// }
+  //This is the click handler we add to each button
+  var addClickHandler = function(btn, type){
+    btn.addEventListener("click", () => {
+      //Set all tables to hidden initially
+      hideAllTables();
+      //Unhide only the specific table related to the button
+      removeInvisible(`.${btn.name.replace('btn', '')}`);
+
+    })
+  }
+
+  //Go through each type of button and add a handler for each
+  var addHandlers = function(){
+    var buttonTypes = ['Avg', 'Marks'];
+    buttonTypes.forEach(type => {
+      document.querySelectorAll(`.${type}btn`).forEach(btn =>{
+        addClickHandler(btn, type);
+      });
+    });
+  }
+
+  //Call it right away to give the buttons handlers
+  addHandlers();
   
+  });
